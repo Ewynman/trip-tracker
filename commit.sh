14216e3ab14a223b6f5037dea841f1b432ce87bb
Eddie Wynman <edward.wynman@gmail.com>
2023-05-06 15:29:51 -0400

    Update Navbar and add commit script

---
+++ commit.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Get the commit message from the user
+echo "Enter commit message:"
+read message
+
+# Add all files to staging area
+git add .
+
+# Commit changes with the specified message
+git commit -m "$message"
+
+# Push changes to the remote repository
+git push origin master
+
+echo "Changes pushed to remote repository."
